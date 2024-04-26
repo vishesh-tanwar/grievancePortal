e28ca31f1f816e19915a116f38a04be9b3135dee
vishesh-tanwar <152698158+vishesh-tanwar@users.noreply.github.com>
2024-04-27 04:10:00 +0530

    Update authenticate.js

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -14,9 +14,7 @@ const authenticate=async(req,res,next)=>{
         console.log("deploy :",isverified);
         //const userData = await User.findOne({enrollment_no:isverified.enrollment_no}) ;    // error in this line 
         //console.log(userData) ;
-        // req.token=token;
-        // req.userData=userData;
-        // req.email = userData; 
+        
         req.isverified=isverified;
         req.token=token 
         
@@ -29,36 +27,3 @@ const authenticate=async(req,res,next)=>{
 
 module.exports=authenticate;
 
-// const jwt = require('jsonwebtoken');
-// const User = require('../model/userschema');
-
-// const authenticate = async (req, res, next) => {
-//     try {
-//         const token = req.header("Authorization");
-//         if (!token) {
-//             return res.status(401).json({ message: "Token not provided" });
-//         }
-
-//         const jwttoken = token.replace("Bearer", "").trim();
-//         console.log("Token:", jwttoken);
-
-//         const decodedToken = jwt.verify(jwttoken,"thisismybvicamjwtokenforthelogin");
-//         console.log("Decoded Token:", decodedToken);
-
-//         const userData = await User.findOne({ email: decodedToken.email });
-//         if (!userData) {
-//             return res.status(401).json({ message: "User not found" });
-//         }
-
-//         req.token = token;
-//         req.userData = userData;
-//         req.email = userData.email;
-
-//         next();
-//     } catch (error) {
-//         console.error("Authentication error:", error);
-//         return res.status(401).json({ message: "Token verification failed" });
-//     }
-// };
-
-// module.exports = authenticate;
